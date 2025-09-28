4bab74118d7ad1d2c6ca5f961197bf20c80f16f1
Kajetan <k.olbryt@gmail.com>
2025-09-28 02:10:20 +0000

    Cover ShouldSubtract test

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -6,5 +6,8 @@ std::string calculate(const std::string& command, int first, int second) {
     if(command == "add") {
         result = std::to_string(first + second);
     }
+    if(command == "subtract") {
+        result = std::to_string(first - second);
+    }
     return result;
 }
