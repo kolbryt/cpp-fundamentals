6afec5db13170e116442cc2d183b6397a2bae3ac
Kajetan <k.olbryt@gmail.com>
2025-09-28 02:06:34 +0000

    Cover ShouldAdd test

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,5 +3,8 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     std::string result{"Invalid data"};
+    if(command == "add") {
+        result = std::to_string(first + second);
+    }
     return result;
 }
