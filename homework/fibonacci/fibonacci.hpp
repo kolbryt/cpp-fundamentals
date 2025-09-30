103e6344bf622cff589f029d980707d45e0f24b3
Kajetan <k.olbryt@gmail.com>
2025-09-30 22:57:02 +0000

    Add fibonacci iterative implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int current_number{0};
+    int next_number{1};
+    for (int i{0}; i < sequence; ++i) {
+        int new_number = current_number + next_number;
+        current_number = next_number;
+        next_number = new_number;
+    }
+    return current_number;
 }
 
 int fibonacci_recursive(int sequence) {
