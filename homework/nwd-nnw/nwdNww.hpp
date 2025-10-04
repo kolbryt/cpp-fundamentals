7783537341f7ac26834dbf6a15090cfbbe49552e
Kajetan <k.olbryt@gmail.com>
2025-10-04 16:50:11 +0000

    Cover NWD tests for positive integers of rhs

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs % rhs == 0) {
+        return rhs;
+    }
+    return NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs) {
