62677750ab8c002dd056cc5075509333f78752bb
Kajetan <k.olbryt@gmail.com>
2025-10-12 00:54:56 +0000

    Cover AddEven test for empty vector

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,8 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
+    if (numbers.size() == 0) {
+        return 0;
+    }
     return -1;
 }
