d2e90d4ed9aff4701488dcaf9fd4d172714b01ef
Kajetan <k.olbryt@gmail.com>
2025-10-19 15:12:59 +0000

    Implement sub10 function for raw pointer

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -26,7 +26,9 @@ void add10(std::vector<std::shared_ptr<int>>& vec) {
 }
 
 void sub10(int* const ptr) {
-
+    if (ptr) {
+        *ptr -= 10;
+    }
 }
 
 void sub10(std::vector<std::shared_ptr<int>>& vec) {
